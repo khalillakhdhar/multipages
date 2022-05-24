a35a18582917a943860628a5e959583cd42e2a2e
khalil <khalillakhdharatc@gmail.com>
2022-05-24 13:55:04 +0100

    mt

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Routes ,Route } from 'react-router-dom';
+import Nav from './layout/nav';
+import {
+BrowserRouter as Router,
 
-function App() {
+} from "react-router-dom";
+import About from './components/About';
+import Contact from './components/Contact';
+class App extends Component {
+  /*
+  on a une détection de routage URL <Router></Router>
+  */
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+  <Router> 
+    <Nav/>
+    
+    <h1>Hello App</h1>
+  <div>
+  
+  <Routes>
+  <Route path="/about" element={<About/>}>
+  </Route>
+  <Route path="/contact" element={<Contact/>}></Route>
+  </Routes>
+  </div>
+  </Router>
   );
-}
-
-export default App;
+  }
+  }
+  export default App
